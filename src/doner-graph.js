cd9d7736f960809948c4bc317dd38ac87e5cd094
a <creightons26@gmail.com>
2017-04-29 20:16:08 -0400

    Added a graph card for top doners

--- src/doner-graph.js
+++ src/doner-graph.js
@@ -1,9 +1,55 @@
-const React = require('react');
+const React = require('react'),
+	Chartist = require('Chartist');
 
-function DonerGraph() {
-	return (
-		<div />
-	);
+class DonerGraph extends React.Component {
+	
+	componentDidMount() {
+		const data = {
+		  labels: [
+		  	'Kim M.',
+		  	'Ella T.',
+		  	'Tara H.',
+		  	'Mike Z,',
+		  	'Jill B.',
+		  	'Jerome V.',
+		  	'Mary A.',
+		  	'Lance G.',
+		  	'Una R.',
+		  	'Allison P.',
+		  ],
+		  series: [
+		    [
+		    	42,
+		    	37,
+		    	35,
+		    	31,
+		    	28,
+		    	22,
+		    	20,
+		    	20,
+		    	19,
+		    	15,
+		    ]
+		  ]
+		};
+
+		const options = {
+		  low: 0
+		};
+
+		new Chartist.Bar('#doner-graph', data, options);
+	}
+
+	render() {
+		return (
+			<div className='card'>
+				<div className='card-header'>
+					Top Doners
+				</div>
+				<div id='doner-graph' />
+			</div>
+		);
+	}
 }
 
 module.exports = DonerGraph;
